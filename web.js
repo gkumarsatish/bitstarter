02d088436fe6a598b3fe5fb711befcdfc63584cd
Satish <gkumarsatish@gmail.com>
2013-07-09 19:20:47 +0000

    updated web.js

--- web.js
+++ web.js
@@ -4,8 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var fs = require('fs');
-  var buff = new Buffer(40);
-  buff = fs.readFileSync(index.html);
+  var buff = fs.readFileSync('index.html');
   response.send(buff.toString());
 });
 
